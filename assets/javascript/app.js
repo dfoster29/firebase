f3fe19ebb3f86c3f04c97f6c56c672fe94e0797a
Daniel Foster <danieljfoster87@gmail.com>
2018-04-02 16:16:41 -0400

    firebase function testing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,36 +11,51 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-$("#submit").on("click", function() {
+// Initial Values
+var trainName = "";
+var destination = "";
+var firstTime = "";
+var frequency = "";
 
-  //  Store Click Data to Firebase in a JSON property called clickCount
-  // Note how we are using the Firebase .set() method
+// Capture Button Click
+$("#submit").on("click", function() {
+  // Don't refresh the page!
+  event.preventDefault();
+
+  // YOUR TASK!!!
+  // Code in the logic for storing and retrieving the most recent user.
+  // Don't forget to provide initial data to your Firebase database.
+  trainName = $("#train-name").val().trim();
+  destination = $("#destination").val().trim();
+  firstTime = $("#first-time").val().trim();
+  frequency = $("#frequency").val().trim();
+
   database.ref().set({
-    // clickCount: clickCounter
+    trainName: trainName,
+    destination: destination,
+    firstTime: firstTime,
+    frequency: frequency
   });
+
 });
 
-// MAIN PROCESS + INITIAL CODE
-// --------------------------------------------------------------------------------
-
-// Using .on("value", function(snapshot)) syntax will retrieve the data
-// from the database (both initially and every time something changes)
-// This will then store the data inside the variable "snapshot". We could rename "snapshot" to anything.
+// Firebase watcher + initial loader HINT: .on("value")
 database.ref().on("value", function(snapshot) {
 
-  // Then we console.log the value of snapshot
+  // Log everything that's coming out of snapshot
   console.log(snapshot.val());
+  console.log(snapshot.val().trainName);
+  console.log(snapshot.val().destination);
+  console.log(snapshot.val().firstTime);
+  console.log(snapshot.val().frequency);
 
-  // Then we change the html associated with the number.
-  $("#submit").text(snapshot.val());
+  // Change the HTML to reflect
+  $("#train-name").text(snapshot.val().trainName);
+  $("#destination").text(snapshot.val().destination);
+  $("#first-time").text(snapshot.val().firstTime);
+  $("#frequency").text(snapshot.val().frequency);
 
-  // Then update the clickCounter variable with data from the database.
-  // clickCounter = snapshot.val().clickCount;
-
-  // If there is an error that Firebase runs into -- it will be stored in the "errorObject"
-  // Again we could have named errorObject anything we wanted.
+  // Handle the errors
 }, function(errorObject) {
-
-  // In case of error this will print the error
-  console.log("The read failed: " + errorObject.code);
+  console.log("Errors handled: " + errorObject.code);
 });
