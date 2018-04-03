97e559ff4ba52952b9e14814b12dc22bde3782b7
Daniel Foster <danieljfoster87@gmail.com>
2018-04-02 20:36:20 -0400

    firebase printing data

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,56 +11,80 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-// Initial Values
-var trainName = "";
-var destination = "";
-var firstTime = "";
-var frequency = "";
-
 // Capture Button Click
-$("#submit").on("click", function() {
+$("#submit").on("click", function(event) {
   // Don't refresh the page!
   event.preventDefault();
 
   // YOUR TASK!!!
   // Code in the logic for storing and retrieving the most recent user.
   // Don't forget to provide initial data to your Firebase database.
-  trainName = $("#train-name").val().trim();
-  destination = $("#destination").val().trim();
-  firstTime = $("#first-time").val().trim();
-  frequency = $("#frequency").val().trim();
+  var train = $("#train-name").val().trim();
+  var dest = $("#destination").val().trim();
+  var first = $("#first-time").val().trim();
+  var freq = $("#frequency").val().trim();
 
-  database.ref().push({
-    trainName: trainName,
-    destination: destination,
-    firstTime: firstTime,
-    frequency: frequency
-  });
+  var newTrain = {
+    name: train,
+    destination: dest,
+    firstTime: first,
+    frequency: freq
+  };
+
+  database.ref().push(newTrain);
+
+  console.log(newTrain.name);
+  console.log(newTrain.destination);
+  console.log(newTrain.firstTime);
+  console.log(newTrain.frequency);
 
   $("#train-name").val("");
   $("#destination").val("");
   $("#first-time").val("");
   $("#frequency").val("");
-
 });
 
 // Firebase watcher + initial loader HINT: .on("value")
-database.ref().on("value", function(snapshot) {
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+    // Log everything that's coming out of snapshot
 
-  // Log everything that's coming out of snapshot
-  console.log(snapshot.val());
-  console.log(snapshot.val().trainName);
-  console.log(snapshot.val().destination);
-  console.log(snapshot.val().firstTime);
-  console.log(snapshot.val().frequency);
+    console.log(childSnapshot.val());
 
-  // Change the HTML to reflect
-  $("#train-name").text(snapshot.val().trainName);
-  $("#destination").text(snapshot.val().destination);
-  $("#first-time").text(snapshot.val().firstTime);
-  $("#frequency").text(snapshot.val().frequency);
+    var train = childSnapshot.val().name;
+    var dest = childSnapshot.val().destination;
+    var first = childSnapshot.val().firstTime;
+    var freq = childSnapshot.val().frequency;
 
-  // Handle the errors
-}, function(errorObject) {
-  console.log("Errors handled: " + errorObject.code);
-});
+    console.log(train);
+    console.log(dest);
+    console.log(first);
+    console.log(freq);
+
+    var nextArrival = moment.unix(firstTime).format("HH:mm");
+
+    // Calculate the months worked using hardcore math
+    // To calculate the months worked
+    var minAway = moment().diff(moment.unix(freq, "X"), "months");
+
+    $("#train-table > tbody").append(
+      "<tr><td>" +
+        train +
+        "</td><td>" +
+        dest +
+        "</td><td>" +
+        first +
+        "</td><td>" +
+        freq +
+        "</td><td>" +
+        nextArrival +
+        "</td><td>" +
+        minAway +
+        "</td></tr>"
+    );
+
+    // Handle the errors
+  },
+  function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+  }
+);
